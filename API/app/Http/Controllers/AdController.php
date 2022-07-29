d1d22fa87bc45e385600ae91dad7cca38890dd13
LujainHSmadi <100359593+LujainHSmadi@users.noreply.github.com>
2022-07-29 15:22:46 +0300

    main

--- API/app/Http/Controllers/AdController.php
+++ API/app/Http/Controllers/AdController.php
@@ -24,7 +24,7 @@ class AdController extends Controller
      */
     public function create()
     {
-        
+
     }
 
     /**
@@ -41,23 +41,58 @@ class AdController extends Controller
             'price' => 'required',
             'type' => 'required',
             'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
-            'image_2'=>'image|mimes:jpeg,png,jpg,gif,svg|max:2048',
-            'image_3'=>'image|mimes:jpeg,png,jpg,gif,svg|max:2048',
-            'image_4'=>'image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'image_2' => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'image_3' => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'image_4' => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048',
         ]);
-        $newad = new Ad([
-            'title' => $request->title,
-            'description' => $request->description,
-            'price' => $request->price,
-            'type' => $request->type,
-            'image' => $request->image->store('public/images'),
-            'image_2' => $request->image_2->store('public/images'),
-            'image_3' => $request->image_3->store('public/images'),
-            'image_4' => $request->image_4->store('public/images'),
 
-        ]);
-        $newad->save();
-        return response()->json( $newad );
+        // $newad = new Ad([
+        //     'title' => $request->title,
+        //     'description' => $request->description,
+        //     'price' => $request->price,
+        //     'type' => $request->type,
+        //     'image' => $request->image->store('public/images'),
+        //     'image_2' => $request->image_2->store('public/images'),
+        //     'image_3' => $request->image_3->store('public/images'),
+        //     'image_4' => $request->image_4->store('public/images'),
+
+        // ]);
+        $newad = $request->all();
+
+        if ($request->file('image')) {
+            $file = $request->file('image');
+            $filename = date('YmdHi') . $file->getClientOriginalName();
+            $file->move(public_path('Image'), $filename);
+            $newad['image'] = "$filename";
+
+        }
+
+        if ($request->file('image_2')) {
+            $file = $request->file('image_2');
+            $filename = date('YmdHi') . $file->getClientOriginalName();
+            $file->move(public_path('Image'), $filename);
+            $newad['image_2'] = "$filename";
+
+        }
+
+        if ($request->file('image_3')) {
+            $file = $request->file('image_3');
+            $filename = date('YmdHi') . $file->getClientOriginalName();
+            $file->move(public_path('Image'), $filename);
+            $newad['image_3'] = "$filename";
+
+        }
+
+        if ($request->file('image_4')) {
+            $file = $request->file('image_4');
+            $filename = date('YmdHi') . $file->getClientOriginalName();
+            $file->move(public_path('Image'), $filename);
+            $newad['image_4'] = "$filename";
+
+        }
+
+        $storAd=Ad::create($newad);
+        return response()->json($storAd);
     }
 
     /**
@@ -69,7 +104,7 @@ class AdController extends Controller
     public function show($id)
     {
         $ad = Ad::findOrFail($id);
-        return response()->json( $ad );
+        return response()->json($ad);
     }
 
     /**
@@ -90,9 +125,9 @@ class AdController extends Controller
      * @param  \App\Models\Ad  $ad
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Ad $ad)
     {
-        $ad = Ad::findOrFail($id);
+        // $ad = Ad::findOrFail($id);
         $request->validate([
             'title' => 'required',
             'description' => 'required',
@@ -100,16 +135,52 @@ class AdController extends Controller
             'price' => 'required',
             'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
         ]);
-        $ad->title = $request->get('name');
-        $ad->description = $request->get('description');
-        $ad->type = $request->get('type');
-        $ad->price = $request->get('price');
-        $ad->image = $request->get('image')->store('public/images');
-        $ad->image_2 = $request->get('image_2')->store('public/images');
-        $ad->image_3 = $request->get('image_3')->store('public/images');
-        $ad->image_4 = $request->get('image_4')->store('public/images');
-        $ad->save();
-        return response()->json( $ad );
+        // $ad->title = $request->get('name');
+        // $ad->description = $request->get('description');
+        // $ad->type = $request->get('type');
+        // $ad->price = $request->get('price');
+        // $ad->image = $request->get('image')->store('public/images');
+        // $ad->image_2 = $request->get('image_2')->store('public/images');
+        // $ad->image_3 = $request->get('image_3')->store('public/images');
+        // $ad->image_4 = $request->get('image_4')->store('public/images');
+        // $ad->save();
+
+        $newad = $request->all();
+
+        if ($request->file('image')) {
+            $file = $request->file('image');
+            $filename = date('YmdHi') . $file->getClientOriginalName();
+            $file->move(public_path('Image'), $filename);
+            $newad['image'] = "$filename";
+
+        }
+
+        if ($request->file('image_2')) {
+            $file = $request->file('image_2');
+            $filename = date('YmdHi') . $file->getClientOriginalName();
+            $file->move(public_path('Image'), $filename);
+            $newad['image_2'] = "$filename";
+
+        }
+
+        if ($request->file('image_3')) {
+            $file = $request->file('image_3');
+            $filename = date('YmdHi') . $file->getClientOriginalName();
+            $file->move(public_path('Image'), $filename);
+            $newad['image_3'] = "$filename";
+
+        }
+
+        if ($request->file('image_4')) {
+            $file = $request->file('image_4');
+            $filename = date('YmdHi') . $file->getClientOriginalName();
+            $file->move(public_path('Image'), $filename);
+            $newad['image_4'] = "$filename";
+
+        }
+
+        $addupdate=Ad::update($newad);
+        return response()->json($addupdate);
 
     }
 
@@ -123,6 +194,6 @@ class AdController extends Controller
     {
         $ad = Ad::findOrFail($id);
         $ad->delete();
-        return response()->json( $ad::all() );
+        return response()->json($ad::all());
     }
 }
