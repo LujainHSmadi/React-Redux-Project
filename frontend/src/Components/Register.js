46130b6bc6b9caf6b286616f039e9aab46613733
Esraa-Khalila <82060330+Esraa-Khalila@users.noreply.github.com>
2022-08-08 07:43:08 +0300

    edit login

--- frontend/src/Components/Register.js
+++ frontend/src/Components/Register.js
@@ -21,7 +21,8 @@ const SignUp = () => {
     setUser({ ...User, [name]: value });
   };
 
-  const save = () => {
+  const save = (e) => {
+    e.preventDefault();
     const { name, email, password, role, image, confirm_password } = User;
 
     dispatch(Register({ name, email, password, role, image, confirm_password }))
@@ -38,27 +39,24 @@ const SignUp = () => {
           confirm_password: data.confirm_password,
         });
 
-      //   swal({
-      //     title: "Welcome to RETHOUSE!",
+        swal({
+          title: "Register Success",
+          icon: "warning",
+          button: "sure!",
+        }).then(function () { window.location.href = "/login";});
+        setSubmitted(true);
+      })
 
-      //     icon: "warning",
-      //     button: "sure!",
-      //   }).then(function () {
-      //     window.location.href = "/";
-      //   });
-      //   setSubmitted(true);
-      // })
-
-      // .catch((e) => {
-      //   console.log(e);
-      //   swal({
-      //     title: "SOMETHING WRON!",
-
-      //     icon: "warning",
-      //     button: "wrong!",
-      //   }).then(function () {
-      //     window.location.href = "/signup";
-      //   });
+      .catch((e) => {
+        console.log(e);
+        swal({
+          title: "SOMETHING WRON! ",
+          text: "The password must be at least 8 characters or The email has already been taken!",
+          icon: "warning",
+          button: "wrong!",
+        }).then(function () {
+          window.location.href = "/signup";
+        });
       });
   };
 
@@ -73,61 +71,65 @@ const SignUp = () => {
             alt=""
             class="image-1"
             style={{ height: "600px", width: "550px", left: "-387px" }}
-          />
-          <div class="register">
-            <h3>New Account?</h3>
+          />{" "}
+          <form onSubmit={save}>
+            <div class="register">
+              <h3>New Account?</h3>
 
-            <div class="form-holderr">
-              <span class="lnr lnr-phone-handset"></span>
-              <input
-                type="text"
-                class="form-controll"
-                placeholder=" name"
-                onChange={handleInputChange}
-                value={User.name || ""}
-                name="name"
-              />
-            </div>
-            <div class="form-holderr">
-              <span class="lnr lnr-envelope"></span>
-              <input
-                type="text"
-                class="form-controll"
-                placeholder="Email"
-                onChange={(event) => handleInputChange(event)}
-                value={User.email || ""}
-                required
-                name="email"
-              />
-            </div>
-            <div class="form-holderr">
-              <span class="lnr lnr-lock"></span>
-              <input
-                type="password"
-                class="form-controll"
-                placeholder="Password"
-                onChange={handleInputChange}
-                value={User.password || ""}
-                required
-                name="password"
-              />
-            </div>
-            <div class="form-holderr">
-              <span class="lnr lnr-lock"></span>
-              <input
-                type="password"
-                class="form-controll"
-                placeholder="Confirm Password"
-                onChange={handleInputChange}
-                required
-                value={User.confirm_password || ""}
-                name="confirm_password"
-              />
-            </div>
-            <button class="registerBtn" type="submit" onClick={save}>
-              <span>Register</span>
-            </button>
-          </div>
+              <div class="form-holderr">
+                <span class="lnr lnr-phone-handset"></span>
+                <input
+                  type="text"
+                  class="form-controll"
+                  placeholder=" name"
+                  onChange={handleInputChange}
+                  value={User.name || ""}
+                  name="name"
+                  minlength="2"
+                />
+              </div>
+              <div class="form-holderr">
+                <span class="lnr lnr-envelope"></span>
+                <input
+                  type="email"
+                  class="form-controll"
+                  placeholder="Email"
+                  onChange={(event) => handleInputChange(event)}
+                  value={User.email || ""}
+                  required
+                  name="email"
+                />
+              </div>
+              <div class="form-holderr">
+                <span class="lnr lnr-lock"></span>
+                <input
+                  type="password"
+                  class="form-controll"
+                  placeholder="Password"
+                  onChange={handleInputChange}
+                  value={User.password || ""}
+                  required
+                  name="password"
+                  minlength="8"
+                />
+              </div>
+              <div class="form-holderr">
+                <span class="lnr lnr-lock"></span>
+                <input
+                  type="password"
+                  class="form-controll"
+                  placeholder="Confirm Password"
+                  onChange={handleInputChange}
+                  required
+                  value={User.confirm_password || ""}
+                  name="confirm_password"
+                />
+              </div>
+              <button class="registerBtn" type="submit">
+                <span>Register</span>
+              </button>
+            </div>{" "}
+          </form>
           <img src="" alt="" class="image-2" />
         </div>
       </div>
